5a2a785a74b753fdae74b305cc01e5e8323e933a
jazmin10 <jazminestrada10@gmail.com>
2018-06-29 17:31:58 -0500

    Accepting arguments from user to determine what command to run

--- liri.js
+++ liri.js
@@ -1,6 +1,36 @@
 // ========== GLOBAL VARIABLES ==========
+// Import modules
+var request = require("request");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
+
+var passedArgs = [];
+var command = "";
 
 // ========== FUNCTIONS ==========
 
 // ========== MAIN PROCESSES ==========
+
+// Store passed arguments by making an array of each arg, except node and file name
+passedArgs = process.argv.slice(2);
+
+// Store the command the user wants to run
+command = passedArgs[0];
+
+switch (command) {
+	case "my-tweets":
+		console.log("grab tweets");
+		break;
+	case "spotify-this-song":
+		console.log("grab song info");
+		break;
+	case "movie-this":
+		console.log("grab movie info");
+		break;
+	case "do-what-it-says":
+		console.log("perform random thing");
+		break;
+	default:
+		console.log("oh no, try again!");
+}
